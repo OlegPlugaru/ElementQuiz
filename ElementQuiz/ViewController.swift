71e797a0fde4cd70315ee4023f90937312de2157
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-10 15:52:36 +0300

    before adding modes

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
+enum Mode {
+    case flashCard
+    case quiz
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    
+    var currentElementIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateElement()
         // Do any additional setup after loading the view.
     }
+    
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        
+        answerLabel.text = "?"
+    }
 
-
+    @IBAction func showAnswer(_ sender: Any) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    @IBAction func next(_ sender: Any) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
+    }
+    
 }
 
